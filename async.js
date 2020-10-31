53feee4104fef21f61bad5292aabefe32996524f
Wei Zhang <iusenenow@gmail.com>
2020-10-31 22:29:04 +1100

    async update

--- async.js
+++ async.js
@@ -1,29 +1,42 @@
 // Promise and fetch
-const promise = new Promise((resolve, reject) => {
-  setTimeout(() => resolve('Resolving an asynchronous request. 哈哈哈哈哈哈哈哈哈哈'), 1000)
-})
+// const promise = new Promise((resolve, reject) => {
+//   setTimeout(() => resolve('Resolving an asynchronous request. 哈哈哈哈哈哈哈哈哈哈'), 1000)
+// })
 
-promise.then(response => response + '嚯嚯嚯嚯嚯').then(secondResponse => console.log(secondResponse))
+// promise.then(response => response + '嚯嚯嚯嚯嚯').then(secondResponse => console.log(secondResponse))
 
-function getUsers(onSuccess) {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      if (onSuccess) {
-        resolve([
-          { id: 1, name: 'Jerry' },
-          { id: 2, name: 'Elaine' },
-          { id: 3, name: 'George' }
-        ])
-      } else {
-        reject('Failed to fetch data. 笨蛋')
-      }
-    }, 1000)
-  })
+// function getUsers(onSuccess) {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       if (onSuccess) {
+//         resolve([
+//           { id: 1, name: 'Jerry' },
+//           { id: 2, name: 'Elaine' },
+//           { id: 3, name: 'George' }
+//         ])
+//       } else {
+//         reject('Failed to fetch data. 笨蛋')
+//       }
+//     }, 1000)
+//   })
+// }
+
+// getUsers(true).then(response => console.log(response)).catch(error => console.log(error)).finally(console.log('This runs anyway.'))
+
+// fetch('https://api.github.com/users/iusenenow')
+//   .then(response => response.json())
+//   .then(data => console.log(data))
+//   .catch(error => console.error(error))
+
+// Async & Await
+const getUser = async () => {
+  try {
+    const response = await fetch('https://api.github.com/users/iusenenow')
+    const data = await response.json()
+    console.log(data);
+  } catch (error) {
+    console.error(error);
+  }
 }
 
-getUsers(true).then(response => console.log(response)).catch(error => console.log(error)).finally(console.log('This runs anyway.'))
-
-fetch('https://api.github.com/users/iusenenow')
-  .then(response => response.json())
-  .then(data => console.log(data))
-  .catch(error => console.error(error))
+getUser()
